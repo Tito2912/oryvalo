8c733258f2b32ec38479402925220453eac52e4d
Tito2912 <caron.tony78@outlook.fr>
2026-02-26 00:09:30 +0100

    Update brand name in SiteHeader component

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -4,7 +4,7 @@ export function SiteHeader() {
   return (
     <header className="header">
       <div className="header-inner">
-        <Link className="brand" href="/">SaaS SEO</Link>
+        <Link className="brand" href="/">Oryvalo</Link>
         <nav className="nav" aria-label="Primary">
           <Link href="/online-business-ideas">Start here</Link>
           <Link href="/how-to-start-an-online-business">Roadmap</Link>
