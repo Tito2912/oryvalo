2ef49df982169baca1ab1d9d77de259abe6134d5
Tito2912 <caron.tony78@outlook.fr>
2026-02-26 19:10:13 +0100

    Replace brand link text with logo image

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -1,14 +1,27 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export function SiteHeader() {
   return (
     <header className="header">
       <div className="header-inner">
-        <Link className="brand" href="/">Oryvalo</Link>
+        
+        <Link className="brand" href="/">
+          <Image
+            src="/oryvalo-logo.png"
+            alt="Oryvalo"
+            width={160}
+            height={40}
+            priority
+            className="h-8 w-auto"
+          />
+        </Link>
+
         <nav className="nav" aria-label="Primary">
           <Link href="/online-business-ideas">Start here</Link>
           <Link href="/how-to-start-an-online-business">Roadmap</Link>
         </nav>
+
       </div>
     </header>
   );
